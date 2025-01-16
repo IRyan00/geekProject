303ae84b2ab5d9f7711c257aa5dfe5ab53e23948
thomas <thomas.baullard@gmail.com>
2025-01-16 13:58:02 +0100

    ad controllers

--- src/controllers/adController.js
+++ src/controllers/adController.js
@@ -20,3 +20,27 @@ exports.createAd = async (req, res) => {
         res.status(500).json({ message: `Erreur lors de la création de l'annonce`, error })
     }
 }
+
+//supprimer
+
+exports.deleteAd = async (req, res) => {
+    try {
+        const { titre, description, type, prix, idUser } = req.body;
+        const newAd = await Ad.delete({ titre, description, type, prix, idUser });
+        res.status(201).json({ message: `L'annonce a bien été supprimée` })
+    } catch (error) {
+        res.status(500).json({ message: `Erreur lors de la suppression de l'annonce`, error })
+    }
+}
+
+//modifier
+
+exports.changeAd = async (req, res) => {
+    try {
+        const { titre, description, type, prix, idUser } = req.body;
+        const newAd = await Ad.change({ titre, description, type, prix, idUser });
+        res.status(201).json({ message: `L'annonce a bien été modifiée` })
+    } catch (error) {
+        res.status(500).json({ message: `Erreur lors de la modification de l'annonce`, error })
+    }
+}
